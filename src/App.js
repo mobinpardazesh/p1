29b78a4064cc9c4b7d5f3bbc082b6f9d1bbed1b6
Mobin Pardazesh <mobinpardazesh@gmail.com>
2023-09-15 21:11:22 +0330

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <div className="nav-background">
+                <div className="nav-container">
+
+                    <nav>
+                        <ul className="nav-ul">
+                            <li><a className="nav-link active-link" href="/#">Home</a></li>
+                            <li><a className="nav-link" href="/#">News</a></li>
+                            <li><a className="nav-link" href="/#">Contact</a></li>
+                            <li><a className="nav-link" href="/#">About</a></li>
+
+                        </ul>
+                    </nav>
+                    <header className="nav-header">
+                        <h1><a className="nav-logo" href="/#"> Cando</a></h1>
+                        <h3><a className="nav-logo" href="/#"> School for pottery</a></h3>
+                    </header>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
-export default App;
